3286a5e955c357f0f512a6413c34665c6de5fea3
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2021-10-08 00:35:12 +0800

    only strip debug info, not all

--- meilisearch/build.sh
+++ meilisearch/build.sh
@@ -1,7 +1,9 @@
 ARCH=arm64
 #ARCH=amd64
-MEILISEARCH_VERSION=v0.23.0rc0
-TAG_VERSION=0.4
+MEILISEARCH_VERSION=v0.23.0rc2
+TAG_VERSION=0.5
+
+sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 buildah build-using-dockerfile \
 --override-arch ${ARCH} \
