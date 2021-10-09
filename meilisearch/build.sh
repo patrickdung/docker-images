3e2a6ea928a5eac83fa81a533de65a15c36a81fc
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2021-10-10 04:42:55 +0800

    Meilisearch, using my repo instead of official repo

--- meilisearch/build.sh
+++ meilisearch/build.sh
@@ -1,11 +1,16 @@
 ARCH=arm64
 #ARCH=amd64
-MEILISEARCH_VERSION=v0.23.0rc2
-TAG_VERSION=0.5
+#MEILISEARCH_VERSION=v0.23.0rc2
+MEILISEARCH_VERSION=v0.23.0rc2-rust-cross-build-2
+#SOURCE_BINARY_BASEURL="https://github.com/meilisearch/MeiliSearch/releases/download"
+SOURCE_BINARY_BASEURL="https://github.com/patrickdung/MeiliSearch/releases/download"
+TAG_VERSION=0.6
 
 sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 buildah build-using-dockerfile \
 --override-arch ${ARCH} \
---build-arg ARCH=${ARCH} --build-arg MEILISEARCH_VERSION=${MEILISEARCH_VERSION} \
+--build-arg ARCH=${ARCH} \
+--build-arg MEILISEARCH_VERSION=${MEILISEARCH_VERSION} \
+--build-arg SOURCE_BINARY_BASEURL=${SOURCE_BINARY_BASEURL} \
 -t "patrickdung/meilisearch:${TAG_VERSION}-${ARCH}"
