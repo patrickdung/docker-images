c9ac22fee9c583a63a43c267627a6fc6fe83e2d4
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2021-10-12 22:11:18 +0800

    update for Meilisearch v0.23.0

--- meilisearch/build.sh
+++ meilisearch/build.sh
@@ -1,10 +1,15 @@
 ARCH=arm64
 #ARCH=amd64
-#MEILISEARCH_VERSION=v0.23.0rc2
-MEILISEARCH_VERSION=v0.23.0rc2-rust-cross-build-2
+MEILISEARCH_VERSION=v0.23.0
+#MEILISEARCH_VERSION=v0.23.0rc2-rust-cross-build-2
+
+# Official
 #SOURCE_BINARY_BASEURL="https://github.com/meilisearch/MeiliSearch/releases/download"
-SOURCE_BINARY_BASEURL="https://github.com/patrickdung/MeiliSearch/releases/download"
-TAG_VERSION=0.6
+# My fork for testing meilisearch
+#SOURCE_BINARY_BASEURL="https://github.com/patrickdung/MeiliSearch/releases/download"
+# Repo with scripts only for cross build
+SOURCE_BINARY_BASEURL="https://github.com/patrickdung/MeiliSearch-crossbuild/releases/download"
+TAG_VERSION=0.7
 
 sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
